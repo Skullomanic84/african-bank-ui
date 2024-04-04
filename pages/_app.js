9c67059688f6c108331c64b6f9272312a2647424
Jahmaine Shiloh <55574810+Skullomanic84@users.noreply.github.com>
2024-04-04 19:44:11 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import '../public/assets/css/style.css';
 import "react-modal-video/css/modal-video.css"
 import { useEffect, useState } from "react";
 import Preloader from '../components/elements/Preloader';
- 
+import Script from "next/script";  
 
 function MyApp({ Component, pageProps }) {
     const [loading, setLoading] = useState(false);
@@ -22,14 +22,14 @@ function MyApp({ Component, pageProps }) {
         window.wow.init();
     }, []);
     return (
-        <>
-            {!loading ? (
-                <Component {...pageProps} />
-            ) : (
-                <Preloader />
-            )}
-        </>
-    )
+      <>
+        <Script
+          src="https://karabo-chatbot.s3.eu-west-1.amazonaws.com/script/mvskarabo.js"
+          defer
+        />
+        {!loading ? <Component {...pageProps} /> : <Preloader />}
+      </>
+    );
 }
 
 export default MyApp
