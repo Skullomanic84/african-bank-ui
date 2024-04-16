cd6e4374a268919139f6daff03f90de6cc7dfc49
Jahmaine Shiloh <55574810+Skullomanic84@users.noreply.github.com>
2024-04-16 11:42:03 +0200

    updated

--- pages/index.jsx
+++ pages/index.jsx
@@ -204,14 +204,7 @@ const Home = () => {
         <section className="section-800 bg-grey-60">
           <div className="container">
             <div className="row align-items-center flex-sm-row-reverse">
-              <div className="col-xl-6 col-lg-6 col-md-6">
-                <img
-                  className="d-block"
-                  src="assets\imgs\new-site\retirement.png"
-                  alt="iori"
-                />
-              </div>
-              <div className="col-xl-6 col-lg-6 col-md-6">
+            <div className="col-xl-6 col-lg-6 col-md-6">
                 <h1 className="color-brand-1 mt-15 mb-20">
                   Grow your wealth with African Bank
                 </h1>
@@ -232,6 +225,15 @@ const Home = () => {
                   </Link>
                 </div>
               </div>
+              
+              <div className="col-xl-6 col-lg-6 col-md-6">
+                <img
+                  className="d-block"
+                  src="assets\imgs\new-site\retirement.png"
+                  alt="iori"
+                />
+              </div>
+              
             </div>
           </div>
         </section>
