1d0994e60d0acca50f86f70ae38297b19eda5c11
mcaiphus <mcaiphus@africanbank.co.za>
2024-04-18 09:02:58 +0200

    fix calculator width on mobile

--- components/elements/Breadcrumbs.js
+++ components/elements/Breadcrumbs.js
@@ -1,47 +1,3 @@
-// import Link from 'next/link';
-// import { useRouter } from 'next/router';
-// import React from 'react';
-// import { BsChevronRight } from "react-icons/bs";
-
-// const Breadcrumbs = () => {
-//   const router = useRouter();
-  
-//   // Split the path and filter out empty segments
-//   const pathSegments = router.asPath.split('/').filter(path => path.length > 0);
-
-//   return (
-//     <nav aria-label="breadcrumb"  className="cookies-banner breadcrumb  py-8 px-6 ">
-//       <ol>
-//         {/* Always include the Home link first */}
-        
-//         {pathSegments.map((segment, index) => {
-//           // Reconstruct the URL to this segment
-//           const href = '/' + pathSegments.slice(0, index + 1).join('/');
-//           // Check if it's the last segment to conditionally render as Link or span
-//           const isLast = index === pathSegments.length - 1;
-//           return (
-//             <li key={index}>
-//               {isLast ? (
-//                 <span >Home <BsChevronRight className="breadcrumbs-right-icon"/> {segment}</span> // Removed the chevron for the last item
-//               ) : (
-//                 <>
-//                   <Link href={href}>
-//                     <a>{segment}</a>
-//                   </Link>
-//                   <BsChevronRight className="breadcrumbs-right-icon"/>
-//                 </>
-//               )}
-//             </li>
-//           );
-//         })}
-//       </ol>
-//     </nav>
-//   );
-// };
-
-// export default Breadcrumbs;
-
-
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
@@ -59,7 +15,7 @@ const Breadcrumbs = () => {
         {/* Always include the Home link first */}
         <li className="breadcrumb-item">
           <Link href="/">
-            <a>Home</a>
+            <a className={pathSegments.length === 0 ? "last" : ""}>Home</a>
           </Link>
         </li>
         {pathSegments.length > 0 && pathSegments.map((segment, index) => {
@@ -72,10 +28,11 @@ const Breadcrumbs = () => {
               <BsChevronRight className="breadcrumbs-separator" />
               <li className="breadcrumb-item">
                 {isLast ? (
-                  <span className='last'>{segment}</span> // Display the last segment as plain text
+                  // Apply the 'last' class only to the last segment
+                  <span className="last">{segment}</span>
                 ) : (
                   <Link href={href}>
-                    <a className='last'>{segment}</a>
+                    <a>{segment}</a>
                   </Link>
                 )}
               </li>
@@ -88,6 +45,3 @@ const Breadcrumbs = () => {
 };
 
 export default Breadcrumbs;
-
-
-
