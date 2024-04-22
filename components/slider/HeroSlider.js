d18858c8fa92dd190be174c48b195a988de9c804
mcaiphus <mcaiphus@africanbank.co.za>
2024-04-22 08:13:54 +0200

    button alighnment

--- components/slider/HeroSlider.js
+++ components/slider/HeroSlider.js
@@ -1,104 +1,57 @@
+
 import React from 'react';
-import SwiperCore, { Autoplay, Navigation,Pagination } from "swiper";
+import SwiperCore, { Autoplay, Navigation, Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
-import Link from "next/link";
-import { SlArrowRight } from "react-icons/sl";
-import Submenu from '../../components/elements/Submenu';
+import 'swiper/swiper-bundle.min.css'; // ensure you import Swiper styles
 import BannerAbs from '../../components/elements/BannerAbs';
-SwiperCore.use([Autoplay, Navigation,Pagination]);
+import Submenu from '../../components/elements/Submenu';
+
+// Initialize Swiper modules
+SwiperCore.use([Autoplay, Navigation, Pagination]);
 
 const HeroSlider = () => {
-
-    // const data = [
-    //     { img: "placed.png" },
-    //     { img: "cuebiq.png" },
-    //     { img: "factual.png" },
-    //     { img: "placeiq.png" },
-    //     { img: "airmeet.png" },
-    //     { img: "spen.png" },
-    //     { img: "klippa.png" },
-    //     { img: "matrix.png" }
-    // ];
-
-
-    return (
-      <>
-         <div
-         >
+  return (
+    <>
+      <div>
         <Swiper
           slidesPerView={1}
-          spaceBetween={0}
-          pagination={true}
+          spaceBetween={30} // space between slides
           loop={true}
+          pagination={{
+            clickable: true
+          }}
           autoplay={{
-            delay: 4500000,
+            delay: 5000, // 5 seconds delay
             disableOnInteraction: false,
           }}
-
-
           navigation={{
             prevEl: ".swiper-button-prev-style-3",
             nextEl: ".swiper-button-next-style-3",
           }}
-
-
-          className="swiper-wrapper"
+          className="swiper-container"
         >
-
-        
-          <SwiperSlide className="swiper-slide">
-            <div
-              className="banner-slide-11"
-              style={{
-                backgroundImage: "url(assets/imgs/new-site/Main-banner_BG.jpg)",
-              }}
-            >
-              {/* <div class="banner-slide-11-overlay"></div> */}
-              <BannerAbs/>
-            </div>
-          </SwiperSlide>
-
-
-          <SwiperSlide className="swiper-slide">
-            <div
-              className="banner-slide-11"
-              style={{
-                backgroundImage: "url(assets/imgs/new-site/Main-banner_BG.jpg)",
-              }}
-            >
-              {/* <div class="banner-slide-11-overlay"></div> */}
-              <BannerAbs/>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="swiper-slide">
-            <div
-              className="banner-slide-11"
-              style={{
-                backgroundImage: "url(assets/imgs/new-site/Main-banner_BG.jpg)",
-              }}
-            >
-              {/* <div class="banner-slide-11-overlay"></div> */}
-              <BannerAbs/>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="swiper-slide">
-            <div
-              className="banner-slide-11"
-              style={{
-                backgroundImage: "url(assets/imgs/new-site/Main-banner_BG.jpg)",
-              }}
-            >
-              {/* <div class="banner-slide-11-overlay"></div> */}
-              <BannerAbs/>
-            </div>
-          </SwiperSlide>
+          {['Slide 1', 'Slide 2', 'Slide 3', 'Slide 4'].map((slide, index) => (
+            <SwiperSlide key={index} className="swiper-slide">
+              <div
+                className="banner-slide-11"
+                style={{
+                  backgroundImage: `url(assets/imgs/new-site/Main-banner_BG.jpg)`,
+                  height: '100vh', // Set a height for the slides
+                  backgroundSize: 'cover'
+                }}
+              >
+                <BannerAbs />
+              </div>
+            </SwiperSlide>
+          ))}
+          {/* Navigation buttons */}
+          {/* <div className="swiper-button-prev"></div>
+          <div className="swiper-button-next"></div> */}
         </Swiper>
-        </div>
-        {/* navbar */}
-          <Submenu/>
-      </>
-    );
+      </div>
+      <Submenu />
+    </>
+  );
 };
 
 export default HeroSlider;
-
